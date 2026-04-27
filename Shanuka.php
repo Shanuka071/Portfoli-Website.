b5502fd6ebb94df69e54862f9ae5e220109a4478
Shanuka071 <shanukaaspu@gmail.com>
2026-04-27 08:38:20 +0530

    feat:Advanced and updated the Portfoli Website.

---
+++ Shanuka.php
@@ -0,0 +1,18 @@
+<?php
+$fullname=$_POST['fullname'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+//Database connection
+$conn = new mysqli('localhost','root','','portfoli');
+if($conn->connect_error){
+    die('Conection Failed :'.$conn->connect_error);
+}else{
+    $stmt =$conn->prepare("insert into customer_review(fullname,email,message) values(?,?,?)");
+    $stmt->bind_param("sss",$fullname,$email,$message);
+    $stmt->execute();
+    echo "Your message has been sent successfully.";
+    $stmt->close();
+    $conn->close();
+}
+?>
